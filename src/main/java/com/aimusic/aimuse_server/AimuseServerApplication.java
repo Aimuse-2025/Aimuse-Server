7dfd76cebc500ef80d548ad042e1c70a3b95ea99
choi <choi@choeui-MacBookPro.local>
2025-11-06 17:19:25 +0900

    feat: initial project setup with core dependencies & templates

---
+++ src/main/java/com/aimusic/aimuse_server/AimuseServerApplication.java
@@ -0,0 +1,13 @@
+package com.aimusic.aimuse_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimuseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimuseServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aimusic/aimuse_server/AimuseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimusic.aimuse_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimuseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
